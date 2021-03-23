29c624f3e4c76953f0346a220a2440da9f0efda7
Arseni <69910183+ArseniLaputska@users.noreply.github.com>
2021-03-23 19:01:45 +0300

    1.0

--- HackerNews/SceneDelegate.swift
+++ HackerNews/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  HackerNews
 //
-//  Created by Arseni Laputska on 2/10/21.
+//  Created by Arseni Laputskaon 2/10/21.
 //  Copyright © 2021 Arseni. All rights reserved.
 //
 
